3962395eeda4ed56ea6a7c8340290b2886456f25
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-02-20 17:48:17 +0000

    Updated describe structure in account.test

--- spec/account.test.js
+++ spec/account.test.js
@@ -8,23 +8,24 @@ describe('Account class', () => {
     Transaction.mockClear()
   });
 
-  it('adds a mock deposit to this.transactionHistory', () => {
-    const mockTransaction = new Transaction()
-    mockTransaction.deposit.mockImplementation(() => ['20/2/2023', 100, ""])
-    const userAccount = new Account(0)
-    userAccount.recordTransaction(mockTransaction.deposit())
-    expect(userAccount.transactionHistory).toEqual([['20/2/2023', 100, ""]])
-  })
+  describe('recordTransaction', () => {
+    it('adds a mock deposit to this.transactionHistory', () => {
+      const mockTransaction = new Transaction()
+      mockTransaction.deposit.mockImplementation(() => ['20/2/2023', 100, ""])
+      const userAccount = new Account(0)
+      userAccount.recordTransaction(mockTransaction.deposit())
+      expect(userAccount.transactionHistory).toEqual([['20/2/2023', 100, ""]])
+    })
 
-  it('adds two mock deposits to this. transactionHistory', () => {
-    const firstMockTransaction = new Transaction()
-    const secondMockTransaction = new Transaction()
-    firstMockTransaction.deposit.mockImplementation(() => ['20/2/2023', 100, ""])
-    secondMockTransaction.deposit.mockImplementation(() => ['21/2/2023', 150, ""])
-    const userAccount = new Account(0)
-    userAccount.recordTransaction(firstMockTransaction.deposit())
-    userAccount.recordTransaction(secondMockTransaction.deposit())
-    expect(userAccount.transactionHistory).toEqual([['21/2/2023', 150, ""],['20/2/2023', 100, ""]])
-
+    it('adds two mock deposits to this. transactionHistory', () => {
+      const firstMockTransaction = new Transaction()
+      const secondMockTransaction = new Transaction()
+      firstMockTransaction.deposit.mockImplementation(() => ['20/2/2023', 100, ""])
+      secondMockTransaction.deposit.mockImplementation(() => ['21/2/2023', 150, ""])
+      const userAccount = new Account(0)
+      userAccount.recordTransaction(firstMockTransaction.deposit())
+      userAccount.recordTransaction(secondMockTransaction.deposit())
+      expect(userAccount.transactionHistory).toEqual([['21/2/2023', 150, ""],['20/2/2023', 100, ""]])
+    })
   })
 })
