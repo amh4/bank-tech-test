41aa51758abb1feee8400ba28b161108386bb934
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-02-21 14:43:00 +0000

    refactor of transaction.test

--- spec/transaction.test.js
+++ spec/transaction.test.js
@@ -1,40 +1,38 @@
 const Transaction = require('../lib/transaction')
 
 describe('Transaction class', () => {
+  beforeEach(() => {
+    userTransaction = new Transaction()
+  });
+
   describe('deposit method', () => {
     it('takes an int as a deposit and adds to transaction details', () => {
-      const userTransaction = new Transaction()
       userTransaction.deposit(100)
       expect(userTransaction.currentTransaction).toContain(100)
     })
 
     it('deposit details also include a date', () => {
-      const userTransaction = new Transaction()
       userTransaction.deposit(100)
       expect(userTransaction.currentTransaction).toContain('21/2/2023', 100)
     })
 
     it('deposit details also include a blank space in the debit column', () => {
-      const userTransaction = new Transaction()
       userTransaction.deposit(100)
       expect(userTransaction.currentTransaction).toEqual(['21/2/2023', 100, ""])
     })
 
     it('returns a message if the user does not enter an int', () => {
-      const userTransaction = new Transaction()
       expect(userTransaction.deposit('hello')).toBe('Please enter a number')
     })
   })
 
   describe('withdrawal method', () => {
     it('takes int for withdrawal and adds details to currentTransaction', () => {
-      const userTransaction = new Transaction()
       userTransaction.withdrawal(100)
       expect(userTransaction.currentTransaction).toEqual(['21/2/2023', "", 100])
     })
 
     it('returns a message if input is not an int', () => {
-      const userTransaction = new Transaction()
       expect(userTransaction.withdrawal('hello')).toEqual('Please enter a number')
     })
   })
