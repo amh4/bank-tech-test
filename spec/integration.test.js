216c8aad5a41f567b0284825e18c20412348f196
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-02-21 14:08:52 +0000

    corrected spelling mistake in var name

--- spec/integration.test.js
+++ spec/integration.test.js
@@ -3,7 +3,7 @@ const Transaction = require('../lib/transaction')
 
 describe('Integration testing', () => {
   beforeEach(() => {
-    transcation = new Transaction()
+    transaction = new Transaction()
     userAccount = new Account(0)
     userWithdrawalTestAccount = new Account(1000)
     consoleSpy = jest.spyOn(console, 'log')
@@ -11,15 +11,15 @@ describe('Integration testing', () => {
 
   describe('recordTransaction', () => {
     it('adds a deposit to this.transactionHistory', () => {
-      userAccount.recordTransaction(transcation.deposit(100))
+      userAccount.recordTransaction(transaction.deposit(100))
       expect(userAccount.transactionHistory[0][0]).toEqual('21/2/2023')
       expect(userAccount.transactionHistory[0][1]).toEqual(100)
       expect(userAccount.transactionHistory[0][2]).toEqual("")
     })
 
-    it('adds two deposits to this.transactionHistory', () => {
+    it('records two deposits to this.transactionHistory', () => {
       const transaction2 = new Transaction()
-      userAccount.recordTransaction(transcation.deposit(100))
+      userAccount.recordTransaction(transaction.deposit(100))
       userAccount.recordTransaction(transaction2.deposit(900))
       expect(userAccount.transactionHistory).toHaveLength(2)
       expect(userAccount.transactionHistory[0]).toEqual(['21/2/2023', 900, "",1000])
@@ -27,18 +27,26 @@ describe('Integration testing', () => {
     })
 
     it('checks if the transaction is a credit and adds to current balance if true', () => {
-      userAccount.recordTransaction(transcation.deposit(200))
+      userAccount.recordTransaction(transaction.deposit(200))
       expect(userAccount.currentBalance).toEqual(200)
     })
 
     it('records a withdrawal in the transaction history', () => {
-      userWithdrawalTestAccount.recordTransaction(transcation.withdrawal(100))
+      userWithdrawalTestAccount.recordTransaction(transaction.withdrawal(100))
       expect(userWithdrawalTestAccount.transactionHistory[0]).toEqual(['21/2/2023',"", 100 ,900])
     })
 
     it('updates current balance after withdrawal', () => {
-      userWithdrawalTestAccount.recordTransaction(transcation.withdrawal(100))
+      userWithdrawalTestAccount.recordTransaction(transaction.withdrawal(100))
       expect(userWithdrawalTestAccount.currentBalance).toEqual(900)
     })
+
+    // it('records two withdrawals in transaction history', () => {
+    //   const transaction2 = new Transaction()
+    //   userWithdrawalTestAccount.recordTransaction(transaction.withdrawal(100))
+    //   userWithdrawalTestAccount.recordTransaction(transcation2.withdrawal(300))
+
+
+    // })
   })
 })
