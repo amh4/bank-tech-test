6618511ef0d1bccd610c36cbb5a2e2b77307a9e3
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-02-21 09:46:34 +0000

    first test passed for recording a withdrawal

--- account.js
+++ account.js
@@ -7,13 +7,10 @@ class Account{
   recordTransaction(transaction) {
     if(transaction[1] > 0){
       this.currentBalance += transaction[1]
-      console.log(this.currentBalance)
-      console.log("transaction",transaction)
-      
       transaction.push(this.currentBalance)
-
       this.transactionHistory.unshift(transaction)
-      console.log(this.transactionHistory)
+    } else {
+      this.transactionHistory.unshift(transaction)
     }
   }
 
--- spec/account.test.js
+++ spec/account.test.js
@@ -14,7 +14,9 @@ describe('Account class', () => {
       mockTransaction.deposit.mockImplementation(() => ['20/2/2023', 100, ""])
       const userAccount = new Account(0)
       userAccount.recordTransaction(mockTransaction.deposit())
-      expect(userAccount.transactionHistory[0]).toEqual(['20/2/2023', 100, "", 100])
+      expect(userAccount.transactionHistory[0][0]).toEqual('20/2/2023')
+      expect(userAccount.transactionHistory[0][1]).toEqual(100)
+      expect(userAccount.transactionHistory[0][2]).toEqual("")
     })
 
     it('adds two mock deposits to this. transactionHistory', () => {
@@ -25,7 +27,7 @@ describe('Account class', () => {
       const userAccount = new Account(0)
       userAccount.recordTransaction(firstMockTransaction.deposit())
       userAccount.recordTransaction(secondMockTransaction.deposit())
-      expect(userAccount.transactionHistory).toEqual([['21/2/2023', 150, "", 250],['20/2/2023', 100, "", 100]])
+      expect(userAccount.transactionHistory).toHaveLength(2)
     })
 
     it('checks whether the transaction is a credit and adds to current balance', () => {
@@ -42,6 +44,16 @@ describe('Account class', () => {
       const userAccount = new Account(0)
       userAccount.recordTransaction(mockTransaction.deposit())
       expect(userAccount.transactionHistory).toEqual([['20/2/2023', 100, "", 100]])
+    })
+
+    it('adds a mock withdrawal to this.transactionHistory', () => {
+      const mockTransaction = new Transaction()
+      mockTransaction.withdrawal.mockImplementation(() => ['19/2/2023', "", 100])
+      const userAccount = new Account(0)
+      userAccount.recordTransaction(mockTransaction.withdrawal())
+      expect(userAccount.transactionHistory[0][0]).toEqual('19/2/2023')
+      expect(userAccount.transactionHistory[0][1]).toEqual("")
+      expect(userAccount.transactionHistory[0][2]).toEqual(100)
     })
   })
 })
